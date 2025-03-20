fc02dfc49cbb2730e9caef6007be82e572d5223e
Justin Morrow <justintwomorrow@gmail.com>
2025-03-20 14:59:53 -0600

    getting them blogs

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { AppState } from '@/AppState.js';
+import { blogService } from '@/services/BlogService.js';
+import { logger } from '@/utils/Logger.js';
+import { Pop } from '@/utils/Pop.js';
+import { computed, onMounted } from 'vue';
+
+
+const blogs = computed(() => AppState.blogs)
+
+onMounted(() => {
+  getBlogs()
+})
+
+async function getBlogs() {
+  try {
+    await blogService.getBlogs()
+
+  } catch (error) {
+    Pop.error(error, `no blog man`)
+    logger.error(`no blog boi`, error)
+  }
+}
+</script>
+
+<template>
+  <h1>{{ blogs }}</h1>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ src/services/BlogService.js
@@ -0,0 +1,19 @@
+import { logger } from "@/utils/Logger.js"
+import { api } from "./AxiosService.js"
+import { blog } from "@/models/blog.js"
+import { AppState } from "@/AppState.js"
+
+class BlogService {
+ async getBlogs() {
+   const response = await api.get(`api/blogs`)
+logger.log(`got them blogs`, response.data)
+const blogs = response.data.map(pojo => new blog(pojo))
+AppState.blogs = blogs
+   }
+  }
+
+    
+  
+
+
+export const blogService = new BlogService()
